f6e6f85421973720aed566597e4606813c0ce5be
InVeK94 <68790635+InVeK94@users.noreply.github.com>
2020-12-12 21:31:49 +0100

    Add files via upload
    FR:voici un script juste a start pour ajouter de la neige^^
    
    EN:Here is a script just to start to add snow ^^

---
+++ neige.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+    while true 
+        do
+        
+        SetWeatherTypePersist("XMAS")
+        SetWeatherTypeNowPersist("XMAS")
+        SetWeatherTypeNow("XMAS")
+        SetOverrideWeather("XMAS")
+        
+        Citizen.Wait(1)
+    end
+
+end)
+
+print("^0======================================================================^7")
+print("^0[^4Author^0] ^7:^0 ^0InVek^7")
+print("^0[^3Version^0] ^7:^0 ^01.0^7")
+print("^0[^2Download^0] ^7:^0 ^5https://github.com/InVeK94/Neige/^7")
+print("^0[^1Issues^0] ^7:^0 ^5https://github.com/InVeK94/Neige/^7")
+print("^0======================================================================^7")
